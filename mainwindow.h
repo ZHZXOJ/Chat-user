63630e4d35ad42091f07bdaecb0ad98b9c655358
Violet956 <3050881754@qq.com>
2019-09-21 21:20:52 +0800

    UI重做

--- mainwindow.h
+++ mainwindow.h
@@ -2,8 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QTcpSocket>
-#include <QList>
 
 namespace Ui {
 class MainWindow;
@@ -17,36 +15,8 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void on_actionExit_triggered();
-
-    void on_actionSign_in_triggered();
-
-    void on_actionSign_out_triggered();
-
-    void on_actionsetting_triggered();
-
-    void on_actionconnecting_triggered();
-
-    void on_pushButton_clicked();
-
-    void on_actionTo_User_triggered();
-
-    void on_actiondelet_triggered();
-
-public slots:
-    void onReadReady();
-    void onConnected();
-    void onDisconnected();
-    void onError(QAbstractSocket::SocketError socketError);
-
 private:
     Ui::MainWindow *ui;
-    QTcpSocket tcpSocket;
-
-    int imageIndex;
-
-    quint32 sizePackLast;
 };
 
 #endif // MAINWINDOW_H
